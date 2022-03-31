e1bcfa1674fa244b550be91ccf93f0929204d9b5
Laura Brooks <laura.brooks0@outlook.com>
2022-03-31 18:47:16 +0900

    form php set up

--- mail.php
+++ mail.php
@@ -1,34 +1,32 @@
-<? php
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $message = $_POST['message'];
-  $subject = $_POST['subject'];
-  header('Content-Type: application/json');
-  if ($name === '') {
-    print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
-    exit();
-  }
-  if ($email === '') {
-    print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
-    exit();
-  } else {
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      print json_encode(array('message' => 'Email format invalid.', 'code' => 0));
-      exit();
-    }
-  }
-  if ($subject === '') {
-    print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
-    exit();
-  }
-  if ($message === '') {
-    print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
-    exit();
-  }
-  $content = "From: $name \nEmail: $email \nMessage: $message";
-  $recipient = "laura@laurabrooks.codes";
-  $mailheader = "From: $email \r\n";
-  mail($recipient, $subject, $content, $mailheader) or die("Error!");
-  print json_encode(array('message' => 'Email successfully sent!', 'code' => 1));
-  exit();
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'laura@laurabrooks.codes';
+$subject = 'Your Subject'.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Sucess Message');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Error Message');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
 ?>
